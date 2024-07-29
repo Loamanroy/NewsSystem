13c19eb85378ed1877baf9ba164d71d449eb050e
Loamanroy <lordbarcklay@gmail.com>
2024-07-29 23:11:38 +0300

    Создание новых таблиц Базы Данных

--- NewsSystem/blog/models.py
+++ NewsSystem/blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class NewsSystem(models.Model):
+    title = models.CharField(max_length=36)
+    content = models.TextField(max_length=255, blank=False)
+    time_create = models.DateTimeField(auto_now_add=True)
+    time_update = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=True)
