46cd14d80447f87f67a46b3fe5a5c90086faec3f
Loamanroy <lordbarcklay@gmail.com>
2024-08-02 21:57:18 +0300

    Новые Urls и Вьюхи

--- NewsSystem/blog/urls.py
+++ NewsSystem/blog/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 
-from blog.views import index, about, categories
+from blog.views import index, about, show_post, addpage, contact, login
 
 urlpatterns =[
     path('', index, name="home"),
-    path('about/', about, name="О нас"),
-    path('cats<int:pk>', categories, name="cats_id"),
+    path('about/', about, name="about"),
+    path('addpage/', addpage, name="addpage"),
+    path('contact/', contact, name="contact"),
+    path('login/', login, name="login"),
+    path('post/<int:post_id>/', show_post, name="post")
 ]
--- NewsSystem/blog/views.py
+++ NewsSystem/blog/views.py
@@ -8,6 +8,8 @@ data_db = [
     {'id': 2, 'title': 'Margo Robie', 'content': 'Bio', 'is_published': False},
     {'id': 3, 'title': 'Julie Roberts', 'content': 'Bio', 'is_published': True},
 ]
+
+
 def index(request):
     data = {
         'title': "Главная страница",
@@ -16,8 +18,26 @@ def index(request):
     }
     return render(request, 'blog/index.html', context=data)
 
+
 def about(request):
     return render(request, 'blog/about.html', {'title': 'О сайте'})
 
-def categories(request, cat_id):
-    return HttpResponse(f"<h1>Статьи по категориям<p>id: {cat_id}</p></h1>")
+
+def login(request):
+    return HttpResponse("Авторизация")
+
+
+def contact(request):
+    return HttpResponse("Связаться с нами")
+
+
+def addpage(request):
+    return HttpResponse("Добавление статьи")
+
+
+def show_post(request, post_id):
+    return HttpResponse(f"Отображение статьи с id: {post_id}")
+
+
+def page_not_found(request, exception):
+    return HttpResponse("Страница не найдена")
